2017aa33c435d5de26e108a0ceef27f43cfe02ef
brandonlpanos <brandonlpanos@gmail.com>
2023-06-13 11:00:26 +0200

    made a new script for training the classifiers

---
+++ src/running_classifier.py
@@ -0,0 +1,20 @@
+import json
+import torch
+import torch.nn as nn
+from datasets import ImputationDataset
+from torch.utils.data import DataLoader
+from datasets import find_padding_masks
+from models import TransformerEncoderInputter
+
+transformer_model = TransformerEncoderInputter(feat_dim=35,
+                                    max_len=40,
+                                    d_model=64, 
+                                    n_heads=8, 
+                                    num_layers=1,
+                                    dim_feedforward=256, 
+                                    dropout=0.1, 
+                                    freeze=False)
+transformer_model.float()
+
+# Load pretrained weights 
+transformer_model.load_state_dict(torch.load('../models/inputting_unity_norm.pt'))
